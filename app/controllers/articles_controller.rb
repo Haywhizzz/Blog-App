817204f763da209b6775c187d02db694e7003e13
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-09-21 16:02:23 +0200

    added destroy and create functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])  
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
 
     def new
@@ -25,7 +27,7 @@ class ArticlesController < ApplicationController
         @article.destroy
         flash.notice = "Article '#{@article.title}' Deleted!"
 
-        redirect_to article_path(@article)
+        redirect_to articles_path
     end
 
     def edit
