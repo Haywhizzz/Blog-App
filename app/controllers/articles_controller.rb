7cc24d95bb072ddf5102f0c25d042dafd3ad3262
haywhizzz <haywhizzz@gmail.com>
2020-09-21 12:29:00 +0100

    setup github workflows action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,4 +39,10 @@ class ArticlesController < ApplicationController
 
         redirect_to article_path(@article)
     end
+
+    def show
+        @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    has_many :comments
 end
+
+
